b87de866c6f4e96231bf4eba319b0f687327088f
gbn972 <g_baby972@outlook.com>
2017-07-07 10:59:05 +0200

    Creation du  component  search

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import './App.css';
 import Namelist  from './components/Namelist/index.js';
 
+
 class App extends Component {
 
   
---
+++ src/components/Search/index.js
@@ -0,0 +1,12 @@
+import React, {Component}  from 'react';
+
+
+
+class Namelist extends  Component{
+    render(){
+        return(
+            <div></div>
+        )
+    }
+
+}
